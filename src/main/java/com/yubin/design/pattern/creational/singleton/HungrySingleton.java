777e3999a869e586f5d83ed000de59e6c7761494
yubin <yubin890816@163.com>
2019-02-19 23:19:06 +0800

    序列化破坏单例模式原理及解决方案

--- src/main/java/com/yubin/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/com/yubin/design/pattern/creational/singleton/HungrySingleton.java
@@ -1,12 +1,14 @@
 package com.yubin.design.pattern.creational.singleton;
 
+import java.io.Serializable;
+
 /**
  * 饿汉式单例实现
  *
  * @Author YUBIN
  * @create 2019-02-17
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable {
 
     // 私有化构造函数
     private HungrySingleton() {
@@ -20,6 +22,10 @@ public class HungrySingleton {
     }
 
     public static HungrySingleton getInstance() {
+        return INSTANCE;
+    }
+
+    private Object readResolve() {
         return INSTANCE;
     }
 }
--- src/main/java/com/yubin/design/pattern/creational/singleton/Test.java
+++ src/main/java/com/yubin/design/pattern/creational/singleton/Test.java
@@ -1,5 +1,7 @@
 package com.yubin.design.pattern.creational.singleton;
 
+import java.io.*;
+
 /**
  * 单例设计模式测试类
  *
@@ -8,11 +10,23 @@ package com.yubin.design.pattern.creational.singleton;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        ThreadDemo thread1 = new ThreadDemo();
-        ThreadDemo thread2 = new ThreadDemo();
-        thread1.start();
-        thread2.start();
-        System.out.println("program done");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //ThreadDemo thread1 = new ThreadDemo();
+        //ThreadDemo thread2 = new ThreadDemo();
+        //thread1.start();
+        //thread2.start();
+        //System.out.println("program done");
+
+        HungrySingleton instance = HungrySingleton.getInstance();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton_file"));
+        oos.writeObject(instance);
+
+        File file = new File("singleton_file");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
     }
 }
